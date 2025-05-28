7f26648aa45c74109ea1c09bd3f5265dc2115abb
allenmurfee <allenmurfee123@gmail.com>
2025-05-28 15:44:54 -0400

    added logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,15 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="fixed w-full top-0 z-50 bg-indigo-600 text-white p-4 shadow-md">
+    <header className="fixed w-full top-0 z-50 bg-slate-300 text-white p-4 shadow-md">
       <div className="container mx-auto flex items-center justify-between">
-        <h1 className="text-2xl font-bold">
-          <Link to="/">HybridTrack</Link>
-        </h1>
+        <Link to="/">
+          <img
+            src="/hybridtrack-logo.png" 
+            alt="HybridTrack Logo"
+            className="w-36 h-18"
+          />
+        </Link>
         <nav>
           <ul className="flex space-x-4">
             <li>
